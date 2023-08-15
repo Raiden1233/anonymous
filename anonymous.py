83aac59babd229e25bd40cb1787196547e32b250
Raiden <86716536+Raiden1233@users.noreply.github.com>
2023-08-16 00:16:21 +0530

    Update anonymous.py

--- anonymous.py
+++ anonymous.py
@@ -1,4 +1,4 @@
-@Raiden.tree.command(name='anonymous', description='Send message anonymously') # replace name Raiden with the name of your own  bot. 
+@Raiden.tree.command(name='anonymous', description='Send message anonymously') # replace the name Raiden with the name of your own  bot. 
 async def anonymous(interaction: discord.Interaction, message: str):  # Feel free to change the name anonymous to confess.
    em = discord.Embed(title = f'⌦ 𓊈𒆜 ANONYMOUS MESSAGE 𒆜𓊉࿐☠️ ',description = f'** ♡˗ˏ✎ ➔  {message} **\n\n ╰┈➤Volume: ■■■■■□□□' )
    await interaction.channel.send(embed= em)
